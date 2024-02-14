c96d3b0dba7ce6661b6fd661eb0922bb97e8ce60
ABDULLAH JUTT <113938161+abdullahjuttofc@users.noreply.github.com>
2024-02-14 12:38:48 +0500

    Update config.js

--- config.js
+++ config.js
@@ -12,30 +12,31 @@ global.location="Lahore,Pakistan."
 global.mongodb= process.env.MONGODB_URI || ""
 global.allowJids= process.env.ALLOW_JID || "null" 
 global.blockJids= process.env.BLOCK_JID || "120363023983262391@g.us"
-global.DATABASE_URI=process.env.DATABASE_URL || ""
-global.scan = "https://suhail-md-vtsf.onrender.com/";
+global.DATABASE_URI = process.env.DATABASE_URL || ""
+
 global.timezone= process.env.TZ || process.env.TIME_ZONE || "Asia/Karachi";
 global.github=process.env.GITHUB|| "https://github.com/SuhailTechInfo/Suhail-Md";
-global.gurl  =process.env.GURL  || "https://chat.whatsapp.com/HwhBcKPN1AWKuImlq5kG3X";
-global.website=process.env.GURL || "https://chat.whatsapp.com/HwhBcKPN1AWKuImlq5kG3X" ; 
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/a03584abe46e4813cc352.jpg" ; // SET LOGO FOR IMAGE 
+global.gurl  =process.env.GURL  || "https://whatsapp.com/channel/0029Va8SjGU1noyxsYBA2K2e";
+global.website=process.env.GURL || "https://whatsapp.com/channel/0029Va8SjGU1noyxsYBA2K2e" ; 
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/0cf318c9067160cb911bb.jpg" ; // SET LOGO FOR IMAGE 
 
 
 
-global.devs = "92375879795" // Developer Contact
-global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '923175879795') : "923402054847";"923175879795";
-global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "923184474176";
+global.devs = "923184474176" // Developer Contact
+global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "null";
+global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "923175879795";
 
 
 
 
 //========================= [ BOT SETTINGS ] =========================\\
-global.flush = process.env.FLUSH || "true"; // Make it "true" if bot not responed
-global.style = process.env.STYLE   || "5",  // put '1' to "5" here to check bot styles
+global.style = process.env.STYLE   || '5',  // put '1' to "5" here to check bot styles
 global.flush = process.env.FLUSH   || "false"; // Make it "true" if bot not responed
 global.gdbye = process.env.GOODBYE || "false"; 
-global.wlcm  = process.env.WELCOME || "true";  // Make it "false" for disable WELCOME 
+global.wlcm  = process.env.WELCOME || "false";  // Make it "false" for disable WELCOME 
 
+global.warncount = process.env.WARN_COUNT || 3,
+global.disablepm = process.env.DISABLE_PM || "false",
 global.MsgsInLog = process.env.MSGS_IN_LOG|| "false", // "true"  to see messages , "log" to open logs , "false" to hide logs messages
 global.userImages= process.env.USER_IMAGES|| "text",
 global.waPresence= process.env.WAPRESENCE ||  "set according to your need" ; // 'unavailable' | 'available' | 'composing' | 'recording' | 'paused'
@@ -50,56 +51,29 @@ global.readmessagefrom = process.env.READ_MESSAGE_FROM || "null,923xxxxxxxx";
 //========================= [ AUTO SAVE & READ STATUS ] =========================\\
 global.read_status = process.env.AUTO_READ_STATUS || "false"
 global.save_status = process.env.AUTO_SAVE_STATUS || "false"
-global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null,923xxxxxxxx";
-global.read_status_from =  process.env.READ_STATUS_FROM  || global.read_status_from || "923184474176,923xxxxxxxx";
+global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null,923175879795";
+global.read_status_from =  process.env.READ_STATUS_FROM  ||  "923184474176,923xxxxxxxx";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+global.api_smd = "https://api-smd-1.vercel.app"
+global.scan = "https://suhail-md-vtsf.onrender.com/";
 
 module.exports = {
 
   menu: process.env.MENU || "", /**  Available @MENU @Schemes 1: Aztec_Md, 2: A17_Md, 3: Suhail-Md Default ---------- If Not Choose then it Randomely Pic One Of Them Each time **/
 
-
-
-  HANDLERS : process.env.PREFIX || ".",
-  BRANCH : process.env.BRANCH   || "main",
+  HANDLERS: process.env.PREFIX  || ".",
+  BRANCH  : process.env.BRANCH  || "main",
   VERSION : process.env.VERSION || "V.1.2.8",
-  caption : process.env.CAPTION || "CH HASEEB" , // ```『 ᴘᴏᴡᴇʀᴇᴅ ʙʏ C HASEEB』```", //*『sᴜʙsᴄʀɪʙᴇ • MH MODS OFC』*\n youtube.com/@  mhmodsofc"),
-
+  caption : process.env.CAPTION || "©HASI-ᴍᴅ" , // ```『 ᴘᴏᴡᴇʀᴇᴅ ʙʏ CH HASEEB 』```", //*『sᴜʙsᴄʀɪʙᴇ • MH MODS OFC』*\n youtube.com/@mhmodsofc"),
  
-  author: process.env.PACK_AUTHER || "Suhail-MD",
+  author : process.env.PACK_AUTHER|| "Suhail-MD",
   packname: process.env.PACK_NAME || "♥️",
   botname : process.env.BOT_NAME  || "sᴜʜᴀɪʟ-ᴍᴅ",
   ownername:process.env.OWNER_NAME|| "It'x Suhail",
 
-
-
-
-
   sessionName:process.env.SESSION_ID || "",  // PUT SESSION ID HERE 
-  errorChat: process.env.ERROR_CHAT || "",
-  KOYEB_API: process.env.KOYEB_API  || "false",
-
-
-
-
+  errorChat : process.env.ERROR_CHAT || "",
+  KOYEB_API : process.env.KOYEB_API  || "false",
 
   REMOVE_BG_KEY : process.env.REMOVE_BG_KEY  || "",
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
@@ -107,8 +81,6 @@ module.exports = {
   HEROKU_APP_NAME:process.env.HEROKU_APP_NAME|| "",
   antilink_values:process.env.ANTILINK_VALUES|| "all",
   HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
-
-
 
   aitts_Voice_Id : process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
@@ -162,4 +134,3 @@ fs.watchFile(file, () => { fs.unwatchFile(file);console.log(`Update'${__filename
   //alwaysonline:process.env.WAPRESENCE|| "unavailable", // 'unavailable' | 'online' | 'composing' | 'recording' | 'paused'
   //read_status: process.env.AUTO_READ_STATUS || "false",
   //save_status: process.env.AUTO_SAVE_STATUS || "false",
- 
